fa2cc33abe445e750371c91dc8f2dff06d566aa0
EdwinTannn <89897686+EdwinTannn@users.noreply.github.com>
2022-10-03 22:34:32 +0700

    Add files via upload

---
+++ pmbupj/pembe/pmb.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cover.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    require_once "navbar.php";
+    include "koneksiupj.php";
+?>
+    <header class="header-cover w-100 vh-100 mt-0 d-flex justify-content-center align-item-center">
+        <div class="header-text">
+            <h1>Universitas Pembangunan Jaya</h1>
+            <p >Merupakan sebuah perguruan tinggi swasta di daerah Bintaro yang berdiri pada tahun 2011 dengan akreditasi B dan didukung oleh kelompok usaha Pembangunan Jaya. Kelompok usaha Pembangunan Jaya memiliki 17 usaha yang bergerak dibidang properti, manufaktur, konsultan manajemen, konsultan desain, kontraktor, pariwisata/rekreasi, trading, mekanikal & elektrikal dan pendidikan.</p>
+    </header>
+<div class="container bg-light align-content-center">
+    <h5 style="text-align:center;" class="mt-5">Jalur Seleksi</h5>
+    <p style="text-align:center;">Daftar Jalur Seleksi Masuk</p>
+    <div class="row">
+    <div class="container text-start mt-5">
+              <div class="row">
+                <div class="col-4">
+                <div class="card">
+                  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/302281860_380552370910824_8968547329155042778_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RgVSlvX05ncAX8aEKPn&_nc_ht=scontent-sin6-2.xx&oh=00_AT8lErTCY0k4-ySHHTu_IrqsgUZesmJezsmpdW75E-Pn9g&oe=63405F22" style="width:220px;"class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title fw-bold mt-3">PMB - REGULER RAPOR</h5>
+                    <p class="card-text">Tahun Akademik 2023/2024 - PMB Reguler Rapor Gelombang 1</p>
+                    <a href="form-pendaftaran.php" class="btn btn-primary">Daftar Sekarang</a>
+                  </div>
+                </div>
+                </div>
+                <div class="col-4">
+                <div class="card">
+                  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/302281860_380552370910824_8968547329155042778_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RgVSlvX05ncAX8aEKPn&_nc_ht=scontent-sin6-2.xx&oh=00_AT8lErTCY0k4-ySHHTu_IrqsgUZesmJezsmpdW75E-Pn9g&oe=63405F22" style="width:220px;" class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title fw-bold mt-3">PMB - REGULER PERINGKAT KELAS</h5>
+                    <p class="card-text">Tahun Akademik 2023/2024 - PMB - Reguler Peringkat Kelas Gelombang .</p>
+                    <a href="form-pendaftaran.php" class="btn btn-primary">Daftar Sekarang</a>
+                  </div>
+                </div>
+                </div>
+                <div class="col-4">
+                <div class="card">
+                  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/302281860_380552370910824_8968547329155042778_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RgVSlvX05ncAX8aEKPn&_nc_ht=scontent-sin6-2.xx&oh=00_AT8lErTCY0k4-ySHHTu_IrqsgUZesmJezsmpdW75E-Pn9g&oe=63405F22" style="width:220px;" class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title fw-bold mt-3">PMB - REGULER UJIAN SARINGAN MA</h5>
+                    <p class="card-text">Tahun Akademik 2023/2024 - PMB Reguler Ujian Saringan Masuk Gelombang 1</p>
+                    <a href="form-pendaftaran.php" class="btn btn-primary">Daftar Sekarang</a>
+                  </div>
+                </div>
+                </div>
+                <div class="col-4 mt-5">
+                <div class="card">
+                  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/302281860_380552370910824_8968547329155042778_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RgVSlvX05ncAX8aEKPn&_nc_ht=scontent-sin6-2.xx&oh=00_AT8lErTCY0k4-ySHHTu_IrqsgUZesmJezsmpdW75E-Pn9g&oe=63405F22" style="width:220px;" class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title fw-bold mt-3">PMB-REGULER-ANAK KARYAWAN TETAP JAYA GRUP</h5>
+                    <p class="card-text">Tahun Akademik 2023/2024 - PMB Reguler Ujian Saringan Masuk Gelombang 1</p>
+                    <a href="form-pendaftaran.php" class="btn btn-primary">Daftar Sekarang</a>
+                  </div>
+                </div>
+                </div>
+                <div class="col-4 mt-5">
+                <div class="card">
+                  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/302281860_380552370910824_8968547329155042778_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RgVSlvX05ncAX8aEKPn&_nc_ht=scontent-sin6-2.xx&oh=00_AT8lErTCY0k4-ySHHTu_IrqsgUZesmJezsmpdW75E-Pn9g&oe=63405F22" style="width:220px;" class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title fw-bold mt-3">PMB-REGULER-ANAK KARYAWAN TETAP JAYA GRUP</h5>
+                    <p class="card-text">Tahun Akademik 2023/2024 - PMB Reguler Ujian Saringan Masuk Gelombang 1</p>
+                    <a href="form-pendaftaran.php" class="btn btn-primary">Daftar Sekarang</a>
+                  </div>
+                </div>
+                </div>  
+                <div class="col-4 mt-5">
+                <div class="card">
+                  <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/302281860_380552370910824_8968547329155042778_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RgVSlvX05ncAX8aEKPn&_nc_ht=scontent-sin6-2.xx&oh=00_AT8lErTCY0k4-ySHHTu_IrqsgUZesmJezsmpdW75E-Pn9g&oe=63405F22" style="width:220px;" class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title fw-bold mt-3">BEASISWA PENUH - BEASISWA UPJ UNTUK NEGERI (OSIS)</h5>
+                    <p class="card-text">Tahun Akademik 2023/2024 - PMB Reguler Ujian Saringan Masuk Gelombang 1</p>
+                    <a href="form-pendaftaran.php" class="btn btn-primary">Daftar Sekarang</a>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+    </div>
+    
+       
+</div>
+
+
+</body>
+</html>l
